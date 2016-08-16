40c91a1e6e40bb46fe082ae549229e6a93338831
wupeitao <2859912441Qqq.com>
2016-08-16 23:05:48 +0800

    创建MSwitchButton(80%)

--- app/src/main/java/com/cityant/main/activity/SettingActivity.java
+++ app/src/main/java/com/cityant/main/activity/SettingActivity.java
@@ -20,57 +20,64 @@ public class SettingActivity extends ActivitySupport {
 
     /***
      * 消息通知
+     *
      * @param view
      */
-    public void onMessageClick(View view){
-
+    public void onMessageClick(View view) {
+        mIntent(context, MessageNotifyActivity.class);
     }
 
     /***
      * 黑名单
+     *
      * @param view
      */
-    public void onBlacklistClick(View view){
+    public void onBlacklistClick(View view) {
 
     }
 
     /***
      * 清除缓存
+     *
      * @param view
      */
-    public void onClearcCecheClick(View view){
+    public void onClearcCecheClick(View view) {
 
     }
 
     /***
      * 检查更新
+     *
      * @param view
      */
-    public void onCheckUpdateClick(View view){
+    public void onCheckUpdateClick(View view) {
 
     }
 
     /***
      * 给蚂蚁点个赞
+     *
      * @param view
      */
-    public void onZanClick(View view){
+    public void onZanClick(View view) {
 
     }
 
     /***
      * 账号安全
+     *
      * @param view
      */
-    public void onAccountSecurityClick(View view){
+    public void onAccountSecurityClick(View view) {
 
     }
 
     /****
      * 关于我们
+     *
      * @param view
      */
-    public void onAboutMeClick(View view){
+    public void onAboutMeClick(View view) {
 
     }
 }
