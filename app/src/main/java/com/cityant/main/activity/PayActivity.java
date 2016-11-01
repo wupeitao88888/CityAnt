accb71a59d4b437ef4a4a9b98e3c135d6d8aa1f3
lvfl <939943110@qq.com>
2016-11-01 18:13:35 +0800

    支付页面添加

---
+++ app/src/main/java/com/cityant/main/activity/PayActivity.java
@@ -0,0 +1,21 @@
+package com.cityant.main.activity;
+
+import android.os.Bundle;
+
+import com.cityant.main.R;
+import com.iloomo.base.ActivitySupport;
+
+/**
+* 支付页面
+* @author Lvfl
+* created at 2016/11/1 17:49
+*/
+public class PayActivity extends ActivitySupport {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_list_view);
+        setCtenterTitle(R.string.pay_string);
+    }
+}
