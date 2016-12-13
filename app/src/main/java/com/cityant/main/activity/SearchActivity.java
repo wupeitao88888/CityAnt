e6ec34defecbc20d404c94d55461377f8a74a4b6
lvfl <939943110@qq.com>
2016-12-13 17:27:40 +0800

    搜索页面修改

--- app/src/main/java/com/cityant/main/activity/SearchActivity.java
+++ app/src/main/java/com/cityant/main/activity/SearchActivity.java
@@ -6,24 +6,21 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.BaseAdapter;
 import android.widget.EditText;
-import android.widget.GridView;
 import android.widget.TextView;
-
 import com.cityant.main.R;
 import com.cityant.main.bean.HomeSearch;
-import com.hyphenate.easeui.global.MYAppconfig;
 import com.cityant.main.global.MYTaskID;
+import com.hyphenate.easeui.global.MYAppconfig;
 import com.iloomo.base.ActivitySupport;
 import com.iloomo.net.AsyncHttpPost;
 import com.iloomo.net.ThreadCallBack;
 import com.iloomo.utils.DialogUtil;
-import com.iloomo.utils.ViewHolder;
-
+import com.zhy.view.flowlayout.FlowLayout;
+import com.zhy.view.flowlayout.TagAdapter;
+import com.zhy.view.flowlayout.TagFlowLayout;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -36,9 +33,10 @@ import java.util.Map;
 */
 public class SearchActivity extends ActivitySupport implements ThreadCallBack {
 
-    private GridView search_gridView;
     private List<String> list = new ArrayList<>();
     private EditText search_edit;
+    private TagAdapter tagAdapter;
+    private TagFlowLayout rg_trainer_field;
 
     public static void startActivity(Context context){
         Intent intent = new Intent(context,SearchActivity.class);
@@ -55,12 +53,22 @@ public class SearchActivity extends ActivitySupport implements ThreadCallBack {
 
     private void initView() {
         search_edit = (EditText) findViewById(R.id.search_edit);
-        search_gridView = (GridView) findViewById(R.id.search_gridView);
+        rg_trainer_field = (TagFlowLayout) findViewById(R.id.rg_trainer_field);
         for(int i = 0; i < 10; i++){
             list.add(i+"");
         }
-        SearchGridAdapter adapter = new SearchGridAdapter();
-        search_gridView.setAdapter(adapter);
+        tagAdapter = new TagAdapter(list) {
+            @Override
+            public View getView(FlowLayout parent, int position, Object o) {
+                View view = LayoutInflater.from(SearchActivity.this).inflate(R.layout.search_grid_item_layout, null);
+                TextView field_name_text = (TextView) view.findViewById(R.id.search_text);
+                field_name_text.setOnClickListener(v -> {
+                    searchInternet();
+                });
+                return view;
+            }
+        };
+        rg_trainer_field.setAdapter(tagAdapter);
         search_edit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 
             @Override
@@ -85,15 +93,13 @@ public class SearchActivity extends ActivitySupport implements ThreadCallBack {
         parameter.put("keyword", "手机");
         parameter.put("tag_id", "0");
 
-        AsyncHttpPost httpRequest;
         startHttpRequst("POST", MYAppconfig.HOME_SEARCH, parameter
                 , MYTaskID.HOMESEARCH);
     }
 
     private void startHttpRequst(String requestType, String url,
                                 Map<String, Object> parameter, int resultCode) {
-        AsyncHttpPost httpRequest;
-        httpRequest = new AsyncHttpPost(this, url, parameter, resultCode,
+        new AsyncHttpPost(this, url, parameter, resultCode,
                 HomeSearch.class, context);
     }
 
@@ -116,36 +122,4 @@ public class SearchActivity extends ActivitySupport implements ThreadCallBack {
     public void onCallBackFromThreadError(String resultJson, int resultCode, Object modelClass) {
 
     }
-
-
-    class SearchGridAdapter extends BaseAdapter{
-
-        @Override
-        public int getCount() {
-            return list.size();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return list.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            String string = (String) getItem(i);
-            if(null == view){
-                view = LayoutInflater.from(SearchActivity.this).inflate(R.layout.search_grid_item_layout,null);
-            }
-            TextView search_text = ViewHolder.get(view,R.id.search_text);
-            search_text.setText(string);
-            return view;
-        }
-    }
-
-
 }
